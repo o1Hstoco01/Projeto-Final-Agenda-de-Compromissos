13d8db3a4fbbf573f5dd0677af3115d5e3a65838
sophia.caires <henristoco@gmail.com>
2024-11-27 22:21:50 -0300

    adicionando entitye fazendo re-funcionar

--- src/main/java/br/fatec/product/entities/Product.java
+++ src/main/java/br/fatec/product/entities/Product.java
@@ -2,10 +2,12 @@ package br.fatec.product.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity 
 public class Product implements Serializable{
 
     @Id
--- src/main/java/br/fatec/product/repositories/ProductRepository.java
+++ src/main/java/br/fatec/product/repositories/ProductRepository.java
@@ -1,5 +1,9 @@
 package br.fatec.product.repositories;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.fatec.product.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
     
 }
