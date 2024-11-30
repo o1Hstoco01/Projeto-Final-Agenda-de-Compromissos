fbf9f9bfe938cecfb45da290eb4538cb53b7f37d
Henrique <henristoco@gmail.com>
2024-11-30 20:37:56 -0300

    Mostrando produto by id

--- src/main/java/br/fatec/product/controllers/ProductController.java
+++ src/main/java/br/fatec/product/controllers/ProductController.java
@@ -3,6 +3,8 @@ package br.fatec.product.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,13 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @GetMapping
     public List<Product> getProducts(){
         return service.getAllProducts();
+    }
+
+    @GetMapping("{id}")
+    public Product geProduct(@PathVariable long id){
+        return service.getProductById(id);
     }
 }
