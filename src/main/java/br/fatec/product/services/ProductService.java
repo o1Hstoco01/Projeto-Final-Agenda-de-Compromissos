5c3767a2ec24d221908304e96f71b95663c1daac
sophia.caires <henristoco@gmail.com>
2024-12-01 17:38:41 -0300

    crianção de dto pt 2

--- src/main/java/br/fatec/product/services/ProductService.java
+++ src/main/java/br/fatec/product/services/ProductService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.fatec.product.dto.ProductRequest;
 import br.fatec.product.dto.ProductResponse;
 import br.fatec.product.entities.Product;
 import br.fatec.product.mappers.ProductMapper;
@@ -21,10 +22,11 @@ public class ProductService {
         return repository.findAll().stream().map(p -> ProductMapper.toDTO(p)).collect(Collectors.toList());
     }
 
-    public Product getProductById(long id){
-        return this.repository.findById(id).orElseThrow(
+    public ProductResponse getProductById(long id){
+        Product product = repository.findById(id).orElseThrow(
             () -> new EntityNotFoundException("Produto não cadastrado")
         );
+        return ProductMapper.toDTO(product);
     }
 
     public void delete (long id){
@@ -35,15 +37,16 @@ public class ProductService {
         }
     }
 
-    public Product save(Product product){
-        return repository.save(product);
+    public ProductResponse save(ProductRequest product){
+        Product newProduct = repository.save(ProductMapper.toEntity(product));
+        return ProductMapper.toDTO(newProduct);
     }
 
-    public void update(Product product, long id){
+    public void update(ProductRequest product, long id){
         Product aux = repository.getReferenceById(id);
-        aux.setCategory(product.getCategory());
-        aux.setName(product.getName());
-        aux.setPrice(product.getPrice());
+        aux.setCategory(product.category());
+        aux.setName(product.name());
+        aux.setPrice(product.price());
         repository.save(aux);
     }
     
